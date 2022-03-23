d37a7c1453c079b8934342c3a0405c310f0c9e78
dy <df.creative@gmail.com>
2022-03-23 09:32:37 -0400

    Reorganize module-level parsing

--- src/watify.js
+++ src/watify.js
@@ -1,39 +1,55 @@
 // convert sonr source to wat source
+
+// function-level operators
 const transform = {
   ';': ([_, ...statements], ctx) => {
-    if (ctx?.root) return ['module', ...statements.filter(Boolean).map(n => t(n, {root:true}))]
       // wat doesn't require special order of declarations
       // return ['module', types, imports, functions, tables, memorys, globals, exports, starts, elements, codes, datas]
   },
 
   '=': ([op, l, r]) => {
-    // a(x) = b → (func params, body)
-    if (l[0] === '(') {
-      let [_, fnName, params] = l,
-          // =b, =(a+b.), =(a+b;b.)
-          body = r[0] === '(' ?  r[1][0] === ';' ? r[1].slice(1) : [r[1]] : [r]
 
-      params = params[0] === ',' ? params.slice(1) : params ? [params] : []
-
-      return ['func', fnName, ...params.map(name => ['param', name, 'f64']), ...body.map(t)]
-    }
   },
 
-  '.': ([_, statement], ctx) => {
-    if (ctx?.root) return [
-      'module',
-      statement = t(statement, {root:true}),
-      statement[0]==='func' && ['export', statement[1], statement.slice(0,2)]
-    ]
+  '.': ([_, statement]) => {
     return ['return', statement]
   },
 
   '*': ([op, l, r]) => ['f64.mul', l, r]
 }
 
-const t = (node, ...args) => {
-  return transform[node[0]](node, ...args)
-}
+const t = (node, ...args) => transform[node[0]](node, ...args)
 
-export default node => t(node, {root: true});
+// module-level parser
+export default node => {
+  //a. → //a.;
+  if (node[0] === '.') node = [';', node]
+  let [_, ...nodes] = node
+
+  let statements = [], exports = []
+
+  for (let node of nodes.filter(Boolean)) {
+    // a. → (export a)
+    let exported = false
+    if (node[0] === '.') node = node[1], exported = true
+
+    let [op, l, r] = node
+    // a(x) = b → (func params, body)
+    if (op === '=') {
+      if (l[0] === '(') {
+        let [_, fnName, params] = l,
+            // =b, =(a+b.), =(a+b;b.)
+            body = r[0] === '(' ?  r[1][0] === ';' ? r[1].slice(1) : [r[1]] : [r]
+
+        params = params[0] === ',' ? params.slice(1) : params ? [params] : []
+
+        statements.push(['func', fnName, ...params.map(name => ['param', name, 'f64']), ...body.map(t)])
+
+        if (exported) exports.push(['export', fnName, ['func', fnName]])
+      }
+    }
+  }
+
+  return ['module', ...statements, ...exports]
+};
 
